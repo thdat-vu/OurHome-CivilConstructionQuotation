e8b44568fe120b6490a10cea369a0952c7f79c72
DatSung <datsung.dev@gmail.com>
2024-01-30 06:59:20 +0700

    RevertChange

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/SWP391.CHCQS.OurHomeWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SWP391.CHCQS.OurHomeWeb.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
