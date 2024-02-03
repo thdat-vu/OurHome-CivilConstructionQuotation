5732d7ee1dd4f876acc195f42655f7b7afe65c7a
NguyenDat0809 <datnx080903@gmail.com>
2024-02-03 15:24:26 +0700

    docs: create classes in BondEntitties based on ERD | Date: 3/2/2024
    this is the first prototype of classes of project

---
+++ SWP391.CHCQS/SWP391.CHCQS.Services/BondEntities/StandardQuotation_TaskDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.CHCQS.Services.BondEntities
+{
+    public class StandardQuotation_TaskDetail
+    {
+        [ForeignKey("StandardQuotation")]
+        public string StandardQuotationId { get; set; }
+        public StandardQuotation StandardQuotation { get; set; }
+        [ForeignKey("TaskDetail")]
+        public int TaskDetailId { get; set; }
+        public TaskDetail TaskDetail { get; set; }
+    }
+}
