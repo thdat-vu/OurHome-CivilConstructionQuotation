dd62ce9cd34ac58ca9bca67fd2bf4f38cb1bd9a3
DatSung <datsung.dev@gmail.com>
2024-02-12 22:55:27 +0700

    feat: Added ViewModels folder and ViewModel CLasses | Date: 12/2/2024 | Author: ThanhDat

---
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Engineer/ViewModels/MaterialDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SWP391.CHCQS.OurHomeWeb.Areas.Engineer.ViewModels
+{
+	public class MaterialDetailViewModel
+	{
+		public MaterialViewModel Material { get; set; }
+		public int Quantity { get; set; }
+		public decimal? Price { get; set; }
+	}
+}
---
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Engineer/ViewModels/TaskViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SWP391.CHCQS.OurHomeWeb.Areas.Engineer.ViewModels
+{
+	public class TaskViewModel
+	{
+		public string Id { get; set; } = null!;
+		public string Name { get; set; } = null!;
+		public string? Description { get; set; }
+		public decimal UnitPrice { get; set; }
+		public bool Status { get; set; }
+		public string CategoryId { get; set; } = null!;
+		public string CategoryName { get; set; }
+	}
+}
