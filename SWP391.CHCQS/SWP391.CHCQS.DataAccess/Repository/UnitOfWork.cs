441c25292a36135060b25585535de4fb17e71502
kopami <khoipham2310@gmail.com>
2024-02-14 12:49:40 +0700

    14022024AddRepositoryToDataAccess

---
+++ SWP391.CHCQS/SWP391.CHCQS.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using SWP391.CHCQS.DataAccess.Data;
+using SWP391.CHCQS.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.CHCQS.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SWP391DBContext _db;
+        public IStandardQuotationRepository StandardQuotation;
+        public UnitOfWork(SWP391DBContext db)
+        {
+            _db = db;
+            StandardQuotation = new StandardQuotationRepository(_db);
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
