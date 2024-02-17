ebd6a5ce06f3dfadc799b78e6f9a5be1796e180a
DatSung <datsung.dev@gmail.com>
2024-02-17 17:03:47 +0700

    feat: Add functions view Task List and Material List when Do Quotation of role Engineen

--- SWP391.CHCQS/SWP391.CHCQS.DataAccess/Repository/UnitOfWork.cs
+++ SWP391.CHCQS/SWP391.CHCQS.DataAccess/Repository/UnitOfWork.cs
@@ -20,9 +20,9 @@ namespace SWP391.CHCQS.DataAccess.Repository
         public ITaskRepository Task { get; private set; }
         public IMaterialRepository Material { get; private set; }
         public IConstructDetailRepository ConstructDetail { get; private set; }
-        
+		public ITaskCategoryRepository TaskCategory { get; private set; }
 
-        public UnitOfWork(SWP391DBContext db)
+		public UnitOfWork(SWP391DBContext db)
         {
             _db = db;
             StandardQuotation = new StandardQuotationRepository(_db);
@@ -33,6 +33,7 @@ namespace SWP391.CHCQS.DataAccess.Repository
             Task = new TaskRepository(_db);
             Material = new MaterialRepository(_db);
             ConstructDetail = new ConstructDetailRepository(_db);
+            TaskCategory = new TaskCategoryRepository(_db);
         }
 
         public void Save()
---
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Engineer/Controllers/MaterialController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP391.CHCQS.DataAccess.Repository.IRepository;
+using SWP391.CHCQS.OurHomeWeb.Areas.Engineer.ViewModels;
+
+namespace SWP391.CHCQS.OurHomeWeb.Areas.Engineer.Controllers
+{
+	[Area("Engineer")]
+	public class MaterialController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public MaterialController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		#region
+		/// <summary>
+		/// This function get all CustomeQuotation in Database and return it into JSON, this function ne lib Datatables to show data
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			List<MaterialViewModel> materialVMList = _unitOfWork.Material
+				.GetAll(includeProperties: "Category")
+				.Where(m => m.Status == true)
+				.Select(x => new MaterialViewModel
+				{
+					Id = x.Id,
+					Name = x.Name,
+					InventoryQuantity = x.InventoryQuantity,
+					UnitPrice = x.UnitPrice,
+					Unit = x.Unit,
+					Status = x.Status,
+					CategoryId = x.CategoryId,
+					CategoryName = x.Category.Name
+				})
+				.ToList();
+
+			return Json(new { data = materialVMList });
+		}
+		#endregion
+	}
+}
