8c56061b9ff1958902feb9f9023686cca4949d09
DatSung <datsung.dev@gmail.com>
2024-02-17 21:23:13 +0700

    feat: add view function

--- SWP391.CHCQS/SWP391.CHCQS.DataAccess/Migrations/SWP391DBContextModelSnapshot.cs
+++ SWP391.CHCQS/SWP391.CHCQS.DataAccess/Migrations/SWP391DBContextModelSnapshot.cs
@@ -700,13 +700,8 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         {
                             Id = "CQ001",
                             Acreage = "240m2",
-<<<<<<< Updated upstream
                             Date = new DateTime(2024, 2, 17, 11, 49, 48, 179, DateTimeKind.Local).AddTicks(4815),
                             Description = "I want to build this house for my son and his wife, so i can live with them.",
-=======
-                            Date = new DateTime(2024, 2, 17, 20, 4, 6, 818, DateTimeKind.Local).AddTicks(7134),
-                            Description = "I want to build this house for my son and his wife, so i can live with them",
->>>>>>> Stashed changes
                             EngineerId = "EN001",
                             Location = "Dĩ An, Bình Dương",
                             ManagerId = "MG001",
@@ -1317,9 +1312,6 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         .HasColumnName("customerId")
                         .IsFixedLength();
 
-                    b.Property<DateTime>("Date")
-                        .HasColumnType("datetime2");
-
                     b.Property<string>("Description")
                         .HasMaxLength(500)
                         .HasColumnType("nvarchar(500)")
@@ -1371,11 +1363,9 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         {
                             Id = "PRJ01",
                             CustomerId = "ID001",
-                            Date = new DateTime(2021, 6, 23, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Description = "Nhà ở gia đình",
                             Location = "Phường Hố Nai, thành phố Biên Hòa, tỉnh Đồng Nai",
                             Name = "NHÀ PHỐ CHỊ THẢO TẠI ĐỒNG NAI",
-                            Overview = "Mô hình nhà phố hiện đại vẫn được ưa chuộng nhất qua các năm bởi vẻ đẹp đơn giản nhưng gian trọng và thanh lịch. Và chị Thảo là một trong rất nhiều khách hàng lựa chọn phong cách nhà này.\r\n\r\nVới dịch vụ hoàn thiện nhà trọn gói, chị đã sở hữu cho mình được căn nhà phố hiện đại 1 trệt 2 lầu đầy đủ tiện nghi, đảm bảo thẩm mỹ và độ bền. Cùng khám phá hình ảnh của ngôi nhà qua bài viết sau nhé!",
                             Scale = "1 trệt, 2 lầu",
                             Size = "5x12",
                             Status = true
@@ -1384,11 +1374,9 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         {
                             Id = "PRJ02",
                             CustomerId = "ID002",
-                            Date = new DateTime(2021, 10, 10, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Description = "Nhà ở gia đình",
                             Location = "huyện Bến Lức, tỉnh Long An",
                             Name = "NHÀ PHỐ CHỊ MAI",
-                            Overview = "Mô hình nhà phố hiện đại đang ngày càng trở nên phổ biến ở Việt Nam nhờ vẻ ngoài thẩm mỹ, sang trọng và chi phí xây dựng hợp lý.\r\n\r\nDưới sự hỗ trợ của dịch vụ thi công nhà trọn gói, gia đình Chị Mai tỉnh Long An thành công hoàn thiện ngôi nhà diện tích 105m2 với 1 trệt, 2 lầu theo phong cách hiện đại. Căn nhà không chỉ đáp ứng những công năng cần thiết chủ nhà yêu cầu mà còn đảm bảo yếu tố thẩm mỹ, phong thủy, chi phí tối ưu… ",
                             Scale = "1 trệt, 2 lầu, sân thượng",
                             Size = "5x21",
                             Status = true
@@ -1397,11 +1385,9 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         {
                             Id = "PRJ03",
                             CustomerId = "ID003",
-                            Date = new DateTime(2022, 8, 15, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Description = "Nhà ở gia đình",
                             Location = "Phường An Phú Đông, Quận 12",
                             Name = "NHÀ PHỐ HIỆN ĐẠI 5 TẦNG CỦA ANH MINH",
-                            Overview = "Nhà ở không chỉ để ở, mà còn là nơi thể hiện phong cách, cá tính của gia chủ. Mỗi công trình nhà là một sản phẩm sáng tạo, mang dấu ấn riêng của người kiến trúc sư và chủ nhà.\r\n\r\nTọa lạc tại KDC Nam Long, Quận 12, ngôi nhà của anh Minh là một trong những công trình hiện đại nổi bật trong khu vực. Kiến trúc 5 tầng với kết cấu 1 trệt + 1 lửng + 2 tầng + 1 tum, căn nhà vừa đáp ứng được không gian sống thoải mái cho các thành viên, vừa tối ưu được chi phí thi công. Cùng Hưng Phú Thịnh ngắm nhìn hình ảnh thực tế của công trình này nhé.",
                             Scale = "1 trệt + 1 lửng + 2 lầu + 1 tum, sân thượng",
                             Size = "4.5x18",
                             Status = true
@@ -1410,11 +1396,9 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         {
                             Id = "PRJ04",
                             CustomerId = "ID004",
-                            Date = new DateTime(2022, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Description = "Nhà ở gia đình",
                             Location = "Phường Hiệp Bình Chánh, TP. Thủ Đức",
                             Name = "NHÀ CHỊ NGỌC ANH",
-                            Overview = "Nằm giữa lòng thành phố Thủ Đức, ngôi nhà của chị Ngọc Anh là một điểm nhấn nổi bật với phong cách hiện đại, trẻ trung. Với diện tích gần 85m2, quy mô 1 trệt, 2 lầu và 1 tum sân thượng, căn nhà hoàn thiện không chỉ đáp ứng được công năng mà còn thỏa mãn được những yếu tố về thẩm mỹ, phong thủy, đem lại sự hài lòng cho gia chủ.",
                             Scale = "1 trệt + 2 lầu + 1 tum, sân thượng",
                             Size = "4.35x19.5",
                             Status = true
@@ -1423,11 +1407,9 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                         {
                             Id = "PRJ05",
                             CustomerId = "ID005",
-                            Date = new DateTime(2022, 7, 10, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Description = "Nhà ở gia đình",
                             Location = "Quận 5, TP. HCM",
                             Name = "NHÀ 1 TRỆT 3 LẦU ANH TUẤN ",
-                            Overview = "Thiết kế nhà phố hiện đại 1 trệt 3 lầu sân thượng là lựa chọn hoàn hảo của nhiều gia đình ở thành phố lớn với diện tích xây dựng hạn chế. Mẫu nhà này có thể đáp ứng nhu cầu sinh hoạt tiện nghi, thoải mái nhưng vẫn đảm bảo riêng tư cho gia đình có đến 3 thế hệ cùng sinh sống.",
                             Scale = "Nhà 1 trệt 3 lầu có sân thượng",
                             Size = "4x17",
                             Status = true
@@ -1495,11 +1477,7 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                             ConstructType = "CT2",
                             CustomerId = "ID001",
                             Description = "Customer said that this project must be finished on 3 month",
-<<<<<<< Updated upstream
                             GenerateDate = new DateTime(2024, 2, 17, 11, 49, 48, 179, DateTimeKind.Local).AddTicks(4386),
-=======
-                            GenerateDate = new DateTime(2024, 2, 17, 20, 4, 6, 818, DateTimeKind.Local).AddTicks(6826),
->>>>>>> Stashed changes
                             Location = "Dĩ An, Bình Dương",
                             Status = true
                         },
@@ -1510,11 +1488,7 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                             ConstructType = "CT1",
                             CustomerId = "ID002",
                             Description = "Customer said that this project must be finished on 6 month",
-<<<<<<< Updated upstream
                             GenerateDate = new DateTime(2024, 2, 17, 11, 49, 48, 179, DateTimeKind.Local).AddTicks(4412),
-=======
-                            GenerateDate = new DateTime(2024, 2, 17, 20, 4, 6, 818, DateTimeKind.Local).AddTicks(6836),
->>>>>>> Stashed changes
                             Location = "Quận 5, TP. Hồ Chí Minh",
                             Status = true
                         },
@@ -1525,11 +1499,7 @@ namespace SWP391.CHCQS.DataAccess.Migrations
                             ConstructType = "CT3",
                             CustomerId = "ID003",
                             Description = "Customer said that this project must be finished on 12 month",
-<<<<<<< Updated upstream
                             GenerateDate = new DateTime(2024, 2, 17, 11, 49, 48, 179, DateTimeKind.Local).AddTicks(4415),
-=======
-                            GenerateDate = new DateTime(2024, 2, 17, 20, 4, 6, 818, DateTimeKind.Local).AddTicks(6839),
->>>>>>> Stashed changes
                             Location = "Long Thạnh Mỹ, TP. Thủ Đức",
                             Status = true
                         });
