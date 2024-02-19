b4c60891936707b23024fa3fc70a543377afafd5
Thanh Dat Vu <vuthanhdat181@gmail.com>
2024-02-19 15:43:30 +0700

    19022024 add db script

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Manager/Controllers/MaterialController.cs
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Manager/Controllers/MaterialController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+//DatVT
 namespace SWP391.CHCQS.OurHomeWeb.Areas.Manager.Controllers
 {
+    [Area("Manager")]
     public class MaterialController : Controller
     {
+        //getAll()
         public IActionResult Index()
         {
-            return View();
+
+            //return View();
         }
     }
 }
