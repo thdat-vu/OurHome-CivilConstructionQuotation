d9b3147f233e8920975eafb3810196a44ac8cd50
NguyenDat0809 <datnx080903@gmail.com>
2024-02-22 12:37:39 +0700

    fix: fix demo for datsung | 22/02/2024

---
+++ SWP391.CHCQS/SWP391.CHCQS.DataAccess/Migrations/20240222053527_fixdemofords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SWP391.CHCQS.DataAccess.Migrations
+{
+    public partial class fixdemofords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK__Construct__rooft__52593CB8",
+                table: "ConstructDetail");
+
+            
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConstructDetail_RooftopType_rooftopId",
+                table: "ConstructDetail",
+                column: "rooftopId",
+                principalTable: "RooftopType",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConstructDetail_RooftopType_rooftopId",
+                table: "ConstructDetail");
+
+            
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Construct__rooft__52593CB8",
+                table: "ConstructDetail",
+                column: "rooftopId",
+                principalTable: "RooftopType",
+                principalColumn: "id");
+        }
+    }
+}
