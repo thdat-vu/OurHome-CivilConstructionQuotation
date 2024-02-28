c2014695588c7ccca6ef35bb2debf4f70c718325
Duc-dev-starter <leminhduck@gmail.com>
2024-02-28 23:29:32 +0700

    Update QuotationViewModel | Date: 28/2/2024

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/wwwroot/js/datatables/sellerDTB/QuotationDTB.js
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/wwwroot/js/datatables/sellerDTB/QuotationDTB.js
@@ -5,14 +5,14 @@
 //Need an api method return json to use this
 function loadDataTableRequest() {
     dataTable = $('#tblQuotation').DataTable({
-        "ajax": { url: '/Seller/Quotation/GetAll' },
+        "ajax": { url: '/Seller/Quotation/GetAllQuotation' },
         "columns": [
             { data: 'id', "width": "5%" },
             { data: 'date', "width": "15%" },
-            { data: 'acreage', "width": "15%" },
+            { data: 'acreage', "width": "5%" },
             { data: 'location', "width": "15%" },
             { data: 'description', "width": "35%" },
-            { data: 'status', "width": "15%" },
+            { data: 'status', "width": "5%" },
             {
                 data: 'id',
                 "render": function (data) {
@@ -20,8 +20,9 @@ function loadDataTableRequest() {
                        <a href="/Seller/Quotation/Details?id=${data}" class = "btn btn-primary btn-main border-0 m-1"><i class="bi bi-plus-square"></i> View Details</a>
                     </div >`
                 },
-                "width": "15%"
+                "width": "20%"
             }
         ]
     });
 }
+
