ed733aea2879006af3e5c0f4d54c1f7f9e3f8d91
NguyenDat0809 <datnx080903@gmail.com>
2024-02-28 17:45:07 +0700

    feat: early approve detail and send email
    có lấy lịch sử detail đã bị reject của engineer

--- SWP391.CHCQS/SWP391.CHCQS.Utility/Helpers/EmailSender.cs
+++ SWP391.CHCQS/SWP391.CHCQS.Utility/Helpers/EmailSender.cs
@@ -1,50 +1,116 @@
-﻿using MailKit.Net.Smtp;
+﻿
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace SWP391.CHCQS.Utility.Helpers
 {
-    public class EmailSender : IEmailSender
-    {
-        public string SendGridSecret { get; set; }
+	public class EmailSender : IEmailSender
+	{
+		public string SendGridSecret { get; set; }
 
-        public EmailSender(IConfiguration _config)
-        {
-            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
-        }
+		public EmailSender(IConfiguration _config)
+		{
+			SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+		}
 
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            //var emailToSend = new MimeMessage();
-            //emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
-            //emailToSend.To.Add(MailboxAddress.Parse(email));
-            //emailToSend.Subject = subject;
-            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage};
+		public Task SendEmailAsync(string email, string subject, string htmlMessage)
+		{
+			//var emailToSend = new MimeMessage();
+			//emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+			//emailToSend.To.Add(MailboxAddress.Parse(email));
+			//emailToSend.Subject = subject;
+			//emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage};
 
-            ////send email
-            //using (var emailClient = new SmtpClient())
-            //{
-            //    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            //    emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
-            //    emailClient.Send(emailToSend);
-            //    emailClient.Disconnect(true);
-            //}
+			////send email
+			//using (var emailClient = new SmtpClient())
+			//{
+			//    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+			//    emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
+			//    emailClient.Send(emailToSend);
+			//    emailClient.Disconnect(true);
+			//}
 
-            //return Task.CompletedTask;
+			//return Task.CompletedTask;
 
-            var client = new SendGridClient(SendGridSecret);
-            var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
-            var to = new EmailAddress(email);
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
-            return client.SendEmailAsync(msg);
+			var client = new SendGridClient(SendGridSecret);
+			var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
+			var to = new EmailAddress(email);
+			var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+			return client.SendEmailAsync(msg);
 
-        }
-    }
+		}
+		/*
+		public static Task SendInfoToEmail(string toEmail, string subject, string htmlMessage)
+		{
+
+			string fromMail = "ourhomeswp391@gmail.com";
+			string fromPassword = "kusvqhpurbksspvb";
+
+			MailMessage mess = new MailMessage();
+			mess.From = new MailAddress(fromMail);
+			mess.Subject = subject;
+			mess.To.Add(new MailAddress(toEmail));
+			mess.Body = htmlMessage;
+
+			mess.IsBodyHtml = true;
+
+			var smtp = new SmtpClient("smtp.gmail.com")
+			{
+				Port = 587,
+				Credentials = new NetworkCredential(fromMail, fromPassword),
+				EnableSsl = true
+			};
+			smtp.Send(mess);
+			return Task.CompletedTask;
+		}
+		*/
+		/* HOW TO USE BODY BUILDER TO RENDER HTML
+				var builder = new BodyBuilder();
+		 Set the html version of the message text
+			builder.HtmlBody = string.Format(@"<p>Hey Alice,<br>
+		<p>What are you up to this weekend? Monica is throwing one of her parties on
+		Saturday and I was hoping you could make it.<br>
+		<p>Will you be my +1?<br>
+		<p>-- Joey<br>
+		<center><img src=""cid:{0}""></center>", image.ContentId);
+		We may also want to attach a calendar event for Monica's party...
+		builder.Attachments.Add(@"C:\Users\Joey\Documents\party.ics");
+
+		 Now we just need to set the message body and we're done
+		 message.Body = builder.ToMessageBody();
+		*/
+		public static Task SendInfoToEmail(string toEmail, string subject, string html)
+		{
+			var bodyBuilder = new BodyBuilder();
+			string fromMail = "ourhomeswp391@gmail.com";
+			string fromPassword = "kusvqhpurbksspvb";
+
+			MailMessage mess = new MailMessage();
+			mess.From = new MailAddress(fromMail);
+			mess.Subject = subject;
+			mess.To.Add(new MailAddress(toEmail));
+
+			bodyBuilder.HtmlBody = String.Format(html); ;
+
+			mess.Body = bodyBuilder.HtmlBody;
+
+			mess.IsBodyHtml = true;
+
+			var smtp = new SmtpClient("smtp.gmail.com")
+			{
+				Port = 587,
+				Credentials = new NetworkCredential(fromMail, fromPassword),
+				EnableSsl = true
+			};
+			smtp.Send(mess);
+			return Task.CompletedTask;
+		}
+
+	}
 }
---
+++ SWP391.CHCQS/SWP391.CHCQS.Utility/Helpers/HTMLTemplate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.CHCQS.Utility.Helpers
+{
+	public class HTMLTemplate
+	{
+		public static string Get()
+		{
+			return @"
+    <p>Hello valued customer, we are sending you a response to your request on 24/02/2024. Your total amount due is $XXX, inclusive of the following items:</p>
+    <h2>Task Detail</h2>
+    <table style='border: 1px solid #000'>
+        <thead>
+            <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+                <th>Column 3</th>
+                <th>Column 4</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <!-- Add more rows as needed -->
+        </tbody>
+    </table>
+    <h2>Material Detail</h2>
+    <table style='border: 1px solid #000'>
+        <thead>
+            <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+                <th>Column 3</th>
+                <th>Column 4</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <!-- Add more rows as needed -->
+        </tbody>
+    </table>
+";
+		}
+	}
+}
