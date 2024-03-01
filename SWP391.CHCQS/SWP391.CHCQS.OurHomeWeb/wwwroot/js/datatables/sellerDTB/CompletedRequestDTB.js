6e9b542ea7b25c44f3c0decd1351e4e7d9dea989
Duc-dev-starter <leminhduck@gmail.com>
2024-03-01 23:40:02 +0700

    fix some view | Date: 1/3/2024
    Fix Status of customer's request page, completed request paged, Quotation page

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/wwwroot/js/datatables/sellerDTB/CompletedRequestDTB.js
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/wwwroot/js/datatables/sellerDTB/CompletedRequestDTB.js
@@ -1,19 +1,18 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     loadDataTableRequest();
 });
 
 //Need an api method return json to use this
 function loadDataTableRequest() {
-    dataTable = $('#tblRequest').DataTable({
-        "ajax": { url: '/Seller/Request/GetAll' },
-        "columns": [
+    dataTable = $('#tblRequestCompleted').DataTable({
+        "ajax": { url: '/Seller/Request/GetAllRequestCompleted' },
+        "columns": [    
             { data: 'id', "width": "5%" },
             { data: 'generateDate', "width": "15%" },
             { data: 'cusName', "width": "15%" },
             { data: 'cusGender', "width": "15%" },
             { data: 'cusPhone', "width": "15%" },
-            { data: 'cusEmail', "width": "15%" },   
+            { data: 'cusEmail', "width": "15%" },
             { data: 'constructType', "width": "15%" },
             { data: 'acreage', "width": "15%" },
             { data: 'location', "width": "15%" },
@@ -29,15 +28,6 @@ function loadDataTableRequest() {
                 "width": "15%"
             },
             { data: 'description', "width": "25%" },
-            {
-                data: 'id',
-                "render": function (data) {
-                    return `<div class="w-100 btn-group" role="group">
-                       <a href="/Seller/Quotation/CreateConstructDetails?id=${data}" class = "btn btn-primary btn-main border-0 m-1"><i class="bi bi-plus-square"></i> Create Construct Details</a>
-                    </div >`
-                },
-                "width": "35%"
-            }
         ]
     });
 }
