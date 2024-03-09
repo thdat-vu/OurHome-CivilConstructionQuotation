e5dd931f1ae3537e7f5a144026173c3dd4c4c804
Duc-dev-starter <leminhduck@gmail.com>
2024-03-09 15:44:31 +0700

    Fix Details page | Date: 9/3/2024

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Seller/ViewModels/QuotationViewModel.cs
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Seller/ViewModels/QuotationViewModel.cs
@@ -9,6 +9,12 @@ namespace SWP391.CHCQS.OurHomeWeb.Areas.Seller.ViewModels
         public CustomQuotation CustomQuotation { get; set; }
         public ConstructDetail ConstructDetail { get; set; }
 
+        public string ConstructionName { get; set; }
+        public string InvestmentName { get; set; }
+        public string FoundationName { get; set; }
+        public string BasementName { get; set; }
+        public string RoofName { get; set; }
+
         
 	}
 
