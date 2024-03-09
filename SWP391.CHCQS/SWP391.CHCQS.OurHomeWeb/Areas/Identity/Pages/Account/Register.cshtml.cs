46a56da396bf6a985041bcbc418de381b520a9a9
kopami <khoipham2310@gmail.com>
2024-03-09 11:18:44 +0700

    Update Permission User Of Admin role

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -163,6 +163,7 @@ namespace SWP391.CHCQS.OurHomeWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Email;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
