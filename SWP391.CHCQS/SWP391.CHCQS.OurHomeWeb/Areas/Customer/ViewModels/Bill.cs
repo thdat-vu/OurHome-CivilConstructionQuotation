2e4dc584d55f08f3ff73461e215cf0a33c816d30
kopami <khoipham2310@gmail.com>
2024-03-10 15:10:59 +0700

    Add QuickQuote feature

---
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Customer/ViewModels/Bill.cs
@@ -0,0 +1,10 @@
+﻿namespace SWP391.CHCQS.OurHomeWeb.Areas.Customer.ViewModels
+{
+	public class Bill
+	{
+        public List<BillDetail> BillDetails { get; set; }
+		public double UnitPrice { get; set; }
+		public double TotalPrice { get; set; }
+        public double TotalArea { get; set; }
+    }
+}
---
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Customer/ViewModels/QuickQuoteVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SWP391.CHCQS.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace SWP391.CHCQS.OurHomeWeb.Areas.Customer.ViewModels
+{
+	public class QuickQuoteVM
+	{
+        public ConstructDetail ConstructDetail { get; set; } = null!;
+		public List<SelectListItem> Alleys { get; set; }
+		public IEnumerable<SelectListItem> ConstructionTypes { get; set; }
+		public IEnumerable<SelectListItem> InvestmentTypes { get; set; }
+		public IEnumerable<SelectListItem> FoundationTypes { get; set; }
+		public IEnumerable<SelectListItem> RooftopTypes { get; set; }
+		public IEnumerable<SelectListItem> BasementTypes { get; set; }
+        public Bill ResponseBill { get; set; }
+
+    }
+}
