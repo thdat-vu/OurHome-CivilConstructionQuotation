3bf5e46fb7642bf26543be57348fdea3c917a671
kopami <khoipham2310@gmail.com>
2024-03-11 11:51:35 +0700

    Add validation for QuickQuote 11032024

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Customer/Controllers/MaterialController.cs
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Customer/Controllers/MaterialController.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SWP391.CHCQS.DataAccess.Repository.IRepository;
 
 namespace SWP391.CHCQS.OurHomeWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class MaterialController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public MaterialController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public async Task<IActionResult> Index()
         {
-            return View();
+            var materialCategories = _unitOfWork.MaterialCategory.GetAll(includeProperties: "Materials").ToList();
+            return View(materialCategories);
         }
     }
 }
