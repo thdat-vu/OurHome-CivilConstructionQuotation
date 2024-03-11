50805fb307600f274be11e2a2d02b303962aa423
Duc-dev-starter <leminhduck@gmail.com>
2024-03-11 17:14:58 +0700

    Big update vietnamese view and update some title | Date:11/3/2024

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Seller/Controllers/HomeController.cs
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Seller/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SWP391.CHCQS.Utility;
 
 namespace SWP391.CHCQS.OurHomeWeb.Areas.Seller.Controllers
 {
 	[Area("Seller")]
-	public class HomeController : Controller
+    [Authorize(Roles = SD.Role_Seller)]
+    public class HomeController : Controller
 	{
         public async Task<IActionResult> Index()
         {
