c8d7b4cfc921f2f8a65186ec47b031d2c6144217
NguyenDat0809 <datnx080903@gmail.com>
2024-03-27 15:12:59 +0700

    fix: validation of reason | 27/03/2024

--- SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Manager/ViewModels/RejectReportVM.cs
+++ SWP391.CHCQS/SWP391.CHCQS.OurHomeWeb/Areas/Manager/ViewModels/RejectReportVM.cs
@@ -10,15 +10,16 @@ namespace SWP391.CHCQS.OurHomeWeb.Areas.Manager.ViewModels
         public string SubcriberId { get; set; }
         //chứa id manager đã reject
         public string RejecterId { get; set; }
-        //chứa lý do của manager reject/ chỉnh sửa
-        [Required(ErrorMessage = "Reason is required")]
-        [MinLength(20, ErrorMessage = "The reason should be longer, upper 20 charaters")] //lý do nên có ít nhất 20 ký tự để thể hiện rõ ý 
-        [MaxLength(500, ErrorMessage = "The reason should be shorter, under 500 charaters")] //lý do chỉ nên có nhiều nhất 500 ký tự
+        
         public string ManagerId { get; set; }
 
         public DateTime? RecieveManagerDate { get; set; }
         public string EngineerId { get; set; }
         public DateTime? SubmissionEngineerDate { get; set; }
+        //chứa lý do của manager reject/ chỉnh sửa
+        [Required(ErrorMessage = "Reason is required")]
+        [MinLength(20, ErrorMessage = "The reason should be longer, upper 20 charaters")] //lý do nên có ít nhất 20 ký tự để thể hiện rõ ý 
+        [MaxLength(500, ErrorMessage = "The reason should be shorter, under 500 charaters")] //lý do chỉ nên có nhiều nhất 500 ký tự
         public string Reason { get; set; }
         public DateTime Date { get; set; }
     }
